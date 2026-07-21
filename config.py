1aef58017aeddd8eb947e797cc2a0bc150f783e6
Yogesh <yogesharu2003@gmail.com>
2026-07-21 17:33:47 +0530

    Configure Railway database

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-# Load the .env file
 load_dotenv()
 
 class Config:
-    SECRET_KEY = "ai_interview_secret_key"
-
     DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = int(os.getenv("DB_PORT", 3306))
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_NAME = os.getenv("DB_NAME")
--- database.py
+++ database.py
@@ -1,12 +1,8 @@
-import pymysql
-from config import Config
-
-def get_connection():
-    connection = pymysql.connect(
-        host=Config.DB_HOST,
-        user=Config.DB_USER,
-        password=Config.DB_PASSWORD,
-        database=Config.DB_NAME,
-        cursorclass=pymysql.cursors.DictCursor
-    )
-    return connection
+connection = pymysql.connect(
+    host=Config.DB_HOST,
+    port=Config.DB_PORT,
+    user=Config.DB_USER,
+    password=Config.DB_PASSWORD,
+    database=Config.DB_NAME,
+    cursorclass=pymysql.cursors.DictCursor
+)
